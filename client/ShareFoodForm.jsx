6e34fb985910590c6bb5fadcae82b56cef51fd1d
Zara <annne.lim@hotmail.ca>
2016-06-17 23:47:46 -0700

    worked on shareFoodForm

--- client/ShareFoodForm.jsx
+++ client/ShareFoodForm.jsx
@@ -1,45 +1,33 @@
 var React = require('react');
 
 var ShareFoodForm = React.createClass({
-    propTypes: {
-        //onCommentSubmit: React.PropTypes.func.isRequired
+
+    getInitialState: function() {
+        return {
+            food: '',
+            address: '',
+            image: '',
+            time: '',
+        };
     },
+    
     handleSubmit: function() {
-        var author = this.refs.author.getDOMNode().value.trim();
-        var text = this.refs.text.getDOMNode().value.trim();
-        //this.props.onCommentSubmit({author: author, text: text});
-        handleCommentSubmit({author: author, text: text});
-        this.refs.author.getDOMNode().value = '';
-        this.refs.text.getDOMNode().value = '';
-        return false;
+        
     },
-    handleCommentSubmit: function(comment) {
-        var comments = this.state.data;
-        comments.push(comment);
-        this.setState({data: comments}, function() {
-            // `setState` accepts a callback. To avoid (improbable) race condition,
-            // `we'll send the ajax request right after we optimistically set the new
-            // `state.
-            $.ajax({
-                url: urls.POST.newComment,
-                dataType: 'json',
-                contentType: 'application/json; charset=utf-8',
-                type: 'POST',
-                data: JSON.stringify(comment),
-                success: function(data) {
-                    setTimeout(1000, this.loadCommentsFromServer);
-                }.bind(this),
-                error: function(xhr, status, err) {
-                    console.error(urls.POST.newComment, status, err.toString());
-                }.bind(this)
-            });
-        });
+
+    handleChange: function (name, e) {
+        var change = {};
+        change[name] = e.target.value;
+        this.setState(change);
     },
+
     render: function() {
         return (
             <form className="commentForm" onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="Your name" ref="author" />
-                <input type="text" placeholder="Say something..." ref="text" />
+                <input type="text" placeholder="Food" value={this.state.food} onChange={this.handleChange.bind(this, 'food')} />
+                <input type="text" placeholder="Address" value={this.state.address} onChange={this.handleChange.bind(this, 'address')} />
+                <input type="text" placeholder="Image" value={this.state.image} onChange={this.handleChange.bind(this, 'image')} />
+                <input type="text" placeholder="Time"  value={this.state.time} onChange={this.handleChange.bind(this, 'time')} />
                 <input type="submit" value="Post" />
             </form>
         );
