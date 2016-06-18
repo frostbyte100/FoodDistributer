8f6d51682ff4c183b25b4f03530a49a6500d930f
lfulbel <lfulbel@iastate.edu>
2016-06-17 23:01:42 -0700

    added model for the food event

--- server/models.py
+++ server/models.py
@@ -1,12 +1,16 @@
 import logging
-
 from google.appengine.ext import ndb
-
 
 class Comment(ndb.Model):
     created = ndb.DateTimeProperty(auto_now_add=True)
     text = ndb.StringProperty()
     author = ndb.StringProperty()
+
+class FoodEvent(ndb.Model):
+    foodType = ndb.StringProperty()
+    address = ndb.StringProperty()
+    image64 = ndb.StringProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
 
 def comment_to_json(comment):
     return {
@@ -14,4 +18,12 @@ def comment_to_json(comment):
         'created': str(comment.created),
         'text': comment.text,
         'author': comment.author
+    }
+def FoodEvent_to_json(event):
+    return {
+        'key': event.key.urlsafe(),
+        'food': event.foodType,
+        'address': event.address,
+        'image': event.image64,
+        'time': event.created
     }
